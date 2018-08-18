2d26f216a13642786517e924be8442ff2550db1b
nathanlschneider <nlschneider@gmail.com>
2018-08-17 20:33:20 -0400

    Add eslint automation

--- pm2mon.js
+++ pm2mon.js
@@ -1,31 +1,47 @@
+/*jshint strict:false */
+/*jshint node: true */
+
 var pm2 = require('pm2'),
     express = require('express'),
     app = express();
 
 app.use(express.static("public"));
-
-pm2.connect(function (err) {
-    if (err) {
-        console.error(err);
-        process.exit(2);
-    } else {
-        console.log("Connected to pm2!");
-    }
-})
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.get("/node", function (req, res) {
+pm2.connect(function (err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Connected to pm2!");
+    }
+})
+    ;
+
+app.get("/api/pm2", function (req, res) {
     pm2.list(function (err, list) {
         if (err) {
             console.error(err);
         }
         res.send(list);
-    })
+    });
+});
+
+app.post("/api/post", function (req) {
+    var action = req.body.action;
+    var id = req.body.id;
+
+    if (action === "stop") {
+        pm2.stop(id, function (err) {
+            console.log(err);
+        });
+    }
 });
 
 app.listen(8989);
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
-var app = (function () {
+var app = (function() {
 
-    var appVar = [];
+    "use strict";
+
+    // var appVar = [];
 
     function Module(arrNum, name, id, mode, pid, status, restarts, uptime, cpu, mem) {
         this.arrNum = arrNum;
@@ -9,17 +11,15 @@ var app = (function () {
         this.mode = mode;
         this.pid = pid;
         this.status = status;
-        this.restarts = restarts
+        this.restarts = restarts;
         this.uptime = uptime;
         this.cpu = cpu;
         this.mem = mem;
-
-    };
+    }
 
     Module.prototype = {
 
-        makeElements: function () {
-
+        makeElements: function() {
             var content = document.getElementById('content');
             var contents = document.createElement('div');
             var nameSpan = document.createElement('span');
@@ -45,18 +45,34 @@ var app = (function () {
             modeSpan.classList = "mode";
             pidSpan.innerText = this.pid;
             pidSpan.classList = "pid";
-            statusSpan.innerText = this.status;
+            //statusSpan.innerText = this.status;
             statusSpan.classList = "status";
             restartsSpan.innerText = this.restarts;
             restartsSpan.classList = "restarts";
-            uptimeSpan.innerText = this.uptime;
+            //uptimeSpan.innerText = this.uptime;
             uptimeSpan.classList = "uptime";
-            cpuSpan.innerText = this.cpu + "%";
+            //cpuSpan.innerText = this.cpu + "%";
             cpuSpan.classList = "cpu";
             //memSpan.innerText = this.mem / 1000 + " kb";
             memSpan.classList = "mem";
             btn1.innerText = "Reload";
-            btn2.innerText = "Stop";
+           
+            // btn1.addEventListener('click', function() {
+            //     axios.post('http://localhost:8989/api/post', {
+            //         id: idSpan.innerText,
+            //         action: 'reload'
+            //     });
+            // });
+            
+            // btn2.innerText = "Stop";
+            // btn2.addEventListener('click', function() {
+            //     axios.post('http://localhost:8989/api/post', {
+            //         id: idSpan.innerText,
+            //         action: 'stop'
+            //     });
+            // });
+
+
             btnContainer.classList = "btn";
 
             content.appendChild(contents);
@@ -72,80 +88,104 @@ var app = (function () {
             contents.appendChild(btnContainer);
             btnContainer.appendChild(btn1);
             btnContainer.appendChild(btn2);
- 
-           var calledMem;
-           var calledCpu;
-           var calledUptime;
-           var renderedMem = document.querySelectorAll('.mem');
-           var renderedCpu = document.querySelectorAll('.cpu');
-           var renderedUptime = document.querySelectorAll('.uptime');
 
-           var passNum = this.arrNum;
+            var calledMem = "";
+            var calledCpu = "";
+            var calledUptime = 0;
+            var calledStatus = "";
+            var renderedMem = document.querySelectorAll('.mem');
+            var renderedCpu = document.querySelectorAll('.cpu');
+            var renderedUptime = document.querySelectorAll('.uptime');
+            var renderedStatus = document.querySelectorAll('.status');
 
-           this.dataCall = function () {
-                axios.get('http://localhost:8989/node')
-                .then(function (response) {
-                    
-                    calledMem = response.data[passNum].monit.memory;
-                    calledCpu = response.data[passNum].monit.cpu;    
-                    calledUptime = response.data[passNum].pm2_env.pm_uptime;             
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-               
+            var passNum = this.arrNum;
+
+            this.dataCall = function() {
+                // axios.get('http://192.168.1.10:8989/api/pm2')
+                //     .then(function(response) {
+
+                //         calledMem = response.data[passNum].monit.memory;
+                //         calledCpu = response.data[passNum].monit.cpu;
+                //         calledUptime = response.data[passNum].pm2_env.pm_uptime;
+                //         calledStatus = response.data[passNum].pm2_env.status;
+                //     })
+                //     .catch(function(error) {
+                //         console.log(error);
+                //         var content = document.getElementById('content');
+                //         var contents = document.createElement('h1');
+                //         contents.classList = "error";
+                //         contents.innerText = error;
+                //         content.appendChild(contents);
+                //     });
+
                 renderedMem[passNum + 1].innerText = Math.trunc(calledMem / 1000000) + "mb";
                 renderedCpu[passNum + 1].innerText = calledCpu + "%";
                 renderedUptime[passNum + 1].innerText = msToMin(calledUptime);
-                
-            }
+
+                if (renderedStatus !== "online") {
+                    renderedStatus[passNum + 1].innerText = calledStatus;
+                }
+                if (calledStatus === "online") {
+                    renderedStatus[passNum + 1].style.color = "#7FFF00";
+                } else if (calledStatus === "stopped") {
+                    renderedStatus[passNum + 1].style.color = "grey";
+                    renderedUptime[passNum + 1].innerText = "------------";
+                } else if (calledStatus === "stopping") {
+                    renderedStatus[passNum + 1].style.color = "yellow";
+                } else if (calledStatus === "launching") {
+                    renderedStatus[passNum + 1].style.color = "cyan";
+                } else if (calledStatus === "errored") {
+                    renderedStatus[passNum + 1].style.color = "red";
+                } else if (calledStatus === "one-launch-status") {
+                    renderedStatus[passNum + 1].style.color = "purple";
+                }
+
+            };
             setInterval(this.dataCall.bind(this), 500);
         }
-    }
-
+    };
+ 
     function msToMin(millis) {
         var outString = "";
         var dateNow = Date.now();
         var ms = (dateNow - millis);
-            var d, h, m, s;
-            s = Math.floor(ms / 1000);
-            m = Math.floor(s / 60);
-            s = s % 60;
-            h = Math.floor(m / 60);
-            m = m % 60;
-            d = Math.floor(h / 24);
-            h = h % 24;
+        var d, h, m, s;
+        s = Math.floor(ms / 1000);
+        m = Math.floor(s / 60);
+        s = s % 60;
+        h = Math.floor(m / 60);
+        m = m % 60;
+        d = Math.floor(h / 24);
+        h = h % 24;
 
-            if (d != 0) {
-                outString = d + "d ";
-            }
-            if (h != 0) {
-                outString += h + "h ";
-            }
-            outString += m + "m";
+        if (d !==0) {
+            outString = d + "d ";
+        }
+        if (h !== 0) {
+            outString += h + "h ";
+        }
+        outString += m + "m";
 
+        return outString;
+    }
 
-            return outString;
-          };
-      
-    axios.get('http://localhost:8989/node')
-        .then(function (response) {
-            for (var i = 0; i < response.data.length; i++) {
-                var rd = response.data[i];
-                appVar[i] = new Module(i, rd.name, rd.pm_id, rd.pm2_env.exec_mode, rd.pid,
-                    rd.pm2_env.status, rd.pm2_env.restart_time, rd.pm2_env.pm_uptime,
-                    rd.monit.cpu, rd.monit.memory);
-                appVar[i].makeElements();
-            }
-        })
-        .catch(function (error) {
-            console.log(error);
-            var content = document.getElementById('content');
-            var contents = document.createElement('h1');
-            contents.classList = "error";
-            contents.innerText = error;
-            content.appendChild(contents);
-        });
+    // axios.get('http://192.168.1.10:8989/api/pm2')
+    //     .then(function(response) {
+    //         for (var i = 0; i < response.data.length; i++) {
+    //             var rd = response.data[i];
+    //             appVar[i] = new Module(i, rd.name, rd.pm_id, rd.pm2_env.exec_mode, rd.pid,
+    //                 rd.pm2_env.status, rd.pm2_env.restart_time, rd.pm2_env.pm_uptime,
+    //                 rd.monit.cpu, rd.monit.memory);
+    //             appVar[i].makeElements();
+    //         }
+    //     })
+    //     .catch(function(error) {
+    //         console.log(error);
+    //         var content = document.getElementById('content');
+    //         var contents = document.createElement('h1');
+    //         contents.classList = "error";
+    //         contents.innerText = error;
+    //         content.appendChild(contents);
+    //     });
 
-
-})()
+})(app);
